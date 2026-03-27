151514c132248e5befea66737cec8671b01806dc
papayagoddess <irbth.cl@gmail.com>
2026-03-27 02:32:58 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'accord-v1';
+const cacheName = 'accord-v4';
 const assets = [
   './',
   './index.html',
@@ -6,7 +6,9 @@ const assets = [
   './manifest.json'
 ];
 
+// This forces the new service worker to activate immediately
 self.addEventListener('install', e => {
+  self.skipWaiting(); 
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll(assets);
@@ -14,10 +16,21 @@ self.addEventListener('install', e => {
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+// This deletes old versions of the app from your phone's memory
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
     })
   );
 });
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+    
