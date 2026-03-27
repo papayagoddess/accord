2b4fb3272da177db4e5f4c59aedf37a3bfc619f8
papayagoddess <irbth.cl@gmail.com>
2026-03-27 01:01:06 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'accord-v1';
+const assets = [
+  './',
+  './index.html',
+  './title.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
